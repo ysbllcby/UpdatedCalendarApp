397a87f7b80892436b630e7b41a32182f1169548
ysbllcby <ysabellacubay@gmail.com>
2023-04-21 18:17:19 -0600

    Commit.

--- app/src/main/java/com/example/calendarandroidapp/MainActivity.java
+++ app/src/main/java/com/example/calendarandroidapp/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.calendarandroidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+    private TextView selectedDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+        selectedDateTextView = findViewById(R.id.selectedDateTextView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // Get the selected date
+                Calendar selectedDate = Calendar.getInstance();
+                selectedDate.set(year, month, dayOfMonth);
+
+                // Format the date as a string
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                String dateString = dateFormat.format(selectedDate.getTime());
+
+                // Log the selected date
+                selectedDateTextView.setText("Selected date: " + dateString);
+
+            }
+        });
     }
 }
